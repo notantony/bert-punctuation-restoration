c39d575ce15fc6525ac6157139df8d127b5705a3
notantony <notantony@LAPTOP-J7AR554B.localdomain>
2022-06-12 17:50:30 +0300

    Improve model customization

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -7,15 +7,16 @@ class DenseClassifier(nn.Module):
     def __init__(
         self,
         input_dim: int = 768,
+        hidden_dim: int = 256, 
         n_classes: int = 5,
         dropout: float = 0.2,
     ):
         super().__init__()
         self.clf = nn.Sequential(
-            nn.Linear(input_dim, 256),
+            nn.Linear(input_dim, hidden_dim),
             nn.Dropout(p=dropout),
             nn.ReLU(),
-            nn.Linear(256, n_classes),
+            nn.Linear(hidden_dim, n_classes),
             nn.ReLU(),
         )
 
@@ -42,12 +43,25 @@ class BertClassifier(nn.Module):
         return x
 
 
-def load_bert(model_name) -> torch.nn.Module:
+def load_bert(model_name: str) -> nn.Module:
     model = transformers.BertModel.from_pretrained(model_name)
     return model
 
 
-def load_bert_classifier(bert_model_name: str = 'bert-base-uncased') -> torch.nn.Module:
+def load_bert_classifier(
+    bert_model_name: str = 'bert-base-uncased',
+    classifier_name: str = 'dense-256',
+    bert_emb_dropout: float = 0.2,  
+    clf_dropout: float = 0.2,
+    n_classes: int = 5,
+) -> torch.nn.Module:
     bert = load_bert(bert_model_name)
-    clf = DenseClassifier(768, 5, dropout=0.0)
-    return BertClassifier(bert, clf)
+    
+    if classifier_name == 'dense-256':
+        clf = DenseClassifier(768, 256, n_classes=n_classes, dropout=clf_dropout)
+    elif classifier_name == 'dense-1568':
+        clf = DenseClassifier(1568, 256, n_classes=n_classes, dropout=clf_dropout)
+    else:
+        raise ValueError(f'Unexpected classifier name: {classifier_name}')
+
+    return BertClassifier(bert, clf, dropout=bert_emb_dropout)
