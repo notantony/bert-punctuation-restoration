4321dad0e54eef2e730d7fad62861cfa91b94e7e
notantony <notantony@LAPTOP-J7AR554B.localdomain>
2022-06-20 01:09:15 +0300

    Add device selection util

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -9,3 +9,7 @@ def set_seed(seed: int) -> None:
     np.random.seed(seed)
     torch.manual_seed(seed)
     torch.cuda.manual_seed(seed)
+
+
+def get_default_device() -> str:
+    return 'cuda' if torch.cuda.is_available() else 'cpu'
